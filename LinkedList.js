6b058b1418d3d567ebf0e80bfb2ea4040d7ea963
Brendan Reape <brendanreape88@gmail.com>
2020-09-01 14:14:07 -0700

    drills

---
+++ LinkedList.js
@@ -0,0 +1,83 @@
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  insertAtHead(data) {
+    const newNode = new LinkedListNode(data, this.head);
+    this.head = newNode;
+    this.length++;
+  }
+
+  remove(item) {
+    //If the list is empty
+    if (!this.head) {
+      return null;
+    }
+    //Start at head
+    let currNode = this.head;
+    let previousNode = this.head;
+
+    while (currNode !== null && currNode.value !== item) {
+      previousNode = currNode;
+      currNode = currNode.next;
+    }
+    if (currNode === null) {
+      console.log("Item not found");
+      return;
+    }
+    previousNode.next = currNode.next;
+  }
+
+  find(item) {
+    // Start at the head
+    let currNode = this.head;
+    // If the list is empty
+    if (!this.head) {
+      return null;
+    }
+    // Check for the item
+    while (currNode.value !== item) {
+      /* Return null if it's the end of the list 
+               and the item is not on the list */
+      if (currNode.next === null) {
+        return null;
+      } else {
+        // Otherwise, keep looking
+        currNode = currNode.next;
+      }
+    }
+    // Found it
+    return currNode;
+  }
+
+  getByIndex(index) {
+    if (index < 0 || index >= this.length) return null;
+
+    let current = this.head;
+    for (let i = 0; i < index; 1++) {
+      current = current.next;
+    }
+    return current;
+  }
+
+  print() {
+    let output = "";
+    let current = this.head;
+    while (current) {
+      output = `${output}${current.value} ->`;
+      current = current.next;
+    }
+    console.log(`${output}null`);
+  }
+}
+
+class LinkedListNode {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+module.exports = LinkedList;
